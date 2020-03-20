74b640b0783108dce0801832e7f3962f5b6bc77a
Jeremy Kale Padot <jeremy@padot.us>
2020-03-20 13:32:33 -0700

    add working db with a few issues, routing is not UI friendly

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -3,7 +3,7 @@ namespace HairSalon.Models
   public class Client
   {
     public int ClientId { get; set; }
-    public string Description { get; set; }
+    public string Name { get; set; }
     public int StylistId { get; set; }
     public virtual Stylist Stylist { get; set; }
   }
